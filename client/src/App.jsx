6fa8dd4d101cb0e7be7dc9b385fd037370ef61cd
Michelle Rogers <michelle@merogers.dev>
2022-12-06 16:48:50 -0800

    cleanup app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,16 @@ import Footer from './components/Footer/Footer';
 import Modal from './components/Modal/Modal';
 import Main from './components/Main/Main';
 
-import Login from './components/Login';
-import Register from './components/Register';
-
-import useLoginModal from './hooks/useLoginModal';
-import useRegisterModal from './hooks/useRegisterModal';
-
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
 import Toast from './components/Toast';
+import Login from './components/Login';
+import Register from './components/Register';
+
+import useLoginModal from './hooks/useLoginModal';
+import useRegisterModal from './hooks/useRegisterModal';
 
 // const onLogout = () => {
 //   dispatch(logout())
