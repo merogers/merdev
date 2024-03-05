955d53e5d36a4d3ed84f855b3918fac6b901dc56
Michelle Rogers <michelle@merogers.dev>
2024-03-05 17:54:22 -0600

    feature: add better logging

--- backend/util/logger.util.js
+++ backend/util/logger.util.js
@@ -1,20 +1,35 @@
 const winston = require('winston');
+const WinstonDailyRotateFile = require('winston-daily-rotate-file');
+
+const { combine, timestamp, cli, align, printf } = winston.format;
 
 const env = process.env.NODE_ENV || 'development';
 
+// Production Logging
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD | hh:mm:ss A',
+    }),
+    align(),
+    printf((info) => `[ ${info.timestamp} ] ${info.level}: ${info.message}`),
+  ),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new WinstonDailyRotateFile({
+      filename: './logs/errors-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'error',
+    }),
   ],
 });
 
+// Development Logging to Console
 if (env !== 'production') {
   logger.add(
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: cli(),
+      level: 'info',
     }),
   );
 }
