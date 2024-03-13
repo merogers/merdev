774b5f306ef048ef0318b4d0dc6f288c0d8d0eca
Michelle Rogers <michelle@merogers.dev>
2024-03-13 17:15:08 -0600

    feature: add more security to backend
    add helmet
    add rate limiter for public routes

--- backend/util/email.util.js
+++
@@ -1,9 +0,0 @@
-import { EmailClient } from '@azure/communication-email';
-
-import 'dotenv/config';
-
-const azureConnectionString = process.env.AZURE_CS_STRING;
-
-const emailClient = new EmailClient(azureConnectionString);
-
-export default emailClient;
